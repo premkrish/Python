ff094408d4683213cd8bea8c1ca00aa3602ed73c
premkrish <prem.24.07@gmail.com>
2017-12-21 23:04:59 -0800

    Input to calcuate the area of the circle

--- Lesson 1 - Input/input.py
+++ Lesson 1 - Input/input.py
@@ -1,3 +1,8 @@
-name=input('Enter your name:')
-age=input('Enter your age:')
-print(f'Hello {name} ! you\'re {age} years old')
+# name=input('Enter your name:')
+# age=input('Enter your age:')
+# print(f'Hello {name} ! you\'re {age} years old')
+
+# Calculate the area of a circle
+radius = input('Enter the radius of the circle (m):')
+area = 3.142 * int(radius)**2
+print(f'Area of the circle: {area}')
